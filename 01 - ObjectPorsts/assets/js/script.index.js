6ffe414257ee3039f263f870c6c856b05b0661c9
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2023-08-07 15:03:05 -0300

    feat: edit and remove posts functions

--- 01 - ObjectPorsts/assets/js/script.index.js
+++ 01 - ObjectPorsts/assets/js/script.index.js
@@ -1,18 +1,30 @@
 const posts = [];
-
+let indexPost = -1;
 
 function savePost(){
     const title = document.getElementById("title").value;
     const resume = document.getElementById("resume").value;
     const publisher = document.getElementById("publisher").value;
     const date = document.getElementById("date").value;
+    console.log(title, resume, publisher, date);
 
-   console.log(title, resume, publisher, date);
-
-
+    if(indexPost == -1){
     if(title && resume && publisher && date){
         storePost(title, resume, publisher, date);
-    }
+        clenFields();
+    }}else{
+        if(title && resume && publisher && date){
+            posts[indexPost] = {
+                title,
+                resume,
+                publisher,
+                date
+            };
+            showPosts();
+            indexPost = -1;
+            clenFields();
+        }
+     }
 }
 
 function  storePost(title, resume, publisher, date){
@@ -42,9 +54,33 @@ function showPosts(){
 
              <button onclick="editPost(${index})">Editar</button>
              <button onclick="removePost(${index})">Excluir</button>
-             
+
               </div>
         `;
     })
     document.getElementById("list").innerHTML = showContent;
+}
+
+
+function clenFields(){
+    document.getElementById("title").value = "";
+    document.getElementById("resume").value = "";
+    document.getElementById("publisher").value = "";
+    document.getElementById("date").value = "";
+}
+
+function editPost(index){
+           indexPost = index;
+           const post = posts[index];
+
+           document.getElementById("title").value = post.title;
+           document.getElementById("resume").value = post.resume;
+           document.getElementById("publisher").value = post.publisher;
+           document.getElementById("date").value = post.date;
+}
+
+function removePost(index){
+    posts.splice(index, 1);
+
+    savePost();
 }
